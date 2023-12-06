687c805bfb509c47360409654cc15d376a3b5c81
Christi <123585178+shaqumonti@users.noreply.github.com>
2023-12-06 14:44:09 +0300

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,9 +1,7 @@
 import sys
 
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
-    """ Wrapper function around print from Stackoverflow
-    https://stackoverflow.com/questions/14630288/unicodeencodeerror-charmap-codec-cant-encode-character-maps-to-undefined/16120218
-    """
+
     enc = file.encoding
     if enc == 'UTF-8':
         print(*objects, sep=sep, end=end, file=file)
